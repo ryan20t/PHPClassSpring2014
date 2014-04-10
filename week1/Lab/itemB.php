6540951b40e9e21d090a18df6d628c5fe43a474b
ryan <ryan@10.240.208.195>
2014-04-10 17:15:02 -0400

    Merge origin/master

--- week1/Lab/itemB.php
+++ week1/Lab/itemB.php
@@ -13,24 +13,64 @@ and open the template in the editor.
         <?php
         
         //array_count_values
+        $myArray = array( 30, 20, 10 );
+        print_r(array_count_values($myArray));
         
         //array_flip
+        $flippedArray = array_flip($myArray);
+        echo '<br />';
+        print_r($flippedArray);
         
         //array_key_exists
+        if (array_key_exists( 50, $myArray ) )
+        {
+            echo '<br />',"The 50 element is in the array.";
+        } else {echo '<br />',"50 does not exist";}
         
         //array_map
+        function cube($n)
+        {
+            return($n * $n * $n);
+        }
+        $b = array_map("cube", $myArray);
+        echo '<br />';
+        print_r($b);
         
         //array_rand
+        $randKeys = array_rand( $myArray, 2 );
+        echo '<br />';
+        print_r($randKeys);
         
         //array_push
+        array_push( $myArray, 40 );
+        echo '<br />';
+        print_r($myArray);
         
         //in_array
+        if (in_array( 30, $myArray, false ) )
+        {
+            echo '<br />',"The 30 element is in the array.";
+        } else {echo '<br />',"30 does not exist";}
         
         //shuffle
+        shuffle($myArray);
+        echo '<br />';
+        foreach ($myArray as $key => $val)
+        {
+            echo "shuffled[" , $key , "] = " , $val , '<br />';
+        }
         
         //count/sizeof
+        $numElements = count($myArray);
+        echo '<br />',"The number of elements in my array is ",$numElements;
         
         //sort
+        sort($myArray);
+        echo '<br />';
+        foreach ($myArray as $key => $val)
+        {
+            echo "myArray[" , $key , "] = " , $val , '<br />';
+        }
         
         ?>
     </body>
