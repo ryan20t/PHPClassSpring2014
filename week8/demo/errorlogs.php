d8d8e91fab31f2aa5e07e8166b820491dd5fc2d5
ryan <ryan@10.240.208.188>
2014-05-21 19:53:27 -0400

    Merge origin/master

---
+++ week8/demo/errorlogs.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+        //error_log("this is a test");
+        
+             
+        error_log("\nthis is a test", 3, "logs/errors.log");
+        
+        //print_r($statement->errorInfo());
+        
+        //intval($db->lastInsertId());
+        ?>
+    </body>
+</html>
